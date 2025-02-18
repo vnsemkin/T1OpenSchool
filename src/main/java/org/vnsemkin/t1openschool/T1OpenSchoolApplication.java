d74b26909f0ac9e362a95b4e5a1fff565eb2e425
Vladimir Semkin <vnsemkin@gmail.com>
2025-02-18 13:51:04 +0700

    Initial commit

---
+++ src/main/java/org/vnsemkin/t1openschool/T1OpenSchoolApplication.java
@@ -0,0 +1,13 @@
+package org.vnsemkin.t1openschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1OpenSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1OpenSchoolApplication.class, args);
+    }
+
+}
